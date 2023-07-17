4a1522172391646b52560a557f4b1c69cec91627
LuanVidal <65032589+LuanVidal@users.noreply.github.com>
2023-07-17 10:39:01 -0300

    Add files via upload

--- ARDUINO_MEGA/MEGA.ino
+++ ARDUINO_MEGA/MEGA.ino
@@ -4,6 +4,7 @@ void setup() {
 }
 
 void loop() {
+
   if (Serial3.available()) {
     //Leitura de um byte.
     char data = Serial3.read();
@@ -12,4 +13,19 @@ void loop() {
     //Acrescente o caractere recebido a string de mensagem.
     delay(10);
   }
+
+  float tensao = analogRead(A0);
+  float corrente = analogRead(A1);
+
+  float tensaoMapeada =  tensao * 0.0048875855327468;
+  float correnteMapeada = corrente * 0.0048875855327468;
+
+  // Envia os dados para o ESP8266 via comunicação serial
+  Serial3.print("Tensao:");
+  Serial3.print(tensaoMapeada);
+  Serial3.print("V | Corrente:");
+  Serial3.print(correnteMapeada);
+  Serial3.println("A");
+ 
+  delay(500); // Intervalo de envio dos dados para o ESP8266
 }

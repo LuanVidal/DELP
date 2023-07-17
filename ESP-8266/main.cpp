ecb3ba8f8d84d8e169f049d20bfcfa775029e6b6
LuanVidal <65032589+LuanVidal@users.noreply.github.com>
2023-07-17 10:37:57 -0300

    Add files via upload

--- ESP-8266/main.cpp
+++ ESP-8266/main.cpp
@@ -1,97 +1,100 @@
-#include <ESP8266WiFi.h> 
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+const char* SSID = "TCS_2G";
+const char* PASSWORD = "33331999";
 
-const char* SSID = "SSID-DA-SUA-REDE";                
-const char* PASSWORD = "SENHA-DA-SUA-REDE";
-WiFiClient wifiClient;                        
- 
+WiFiClient wifiClient;
+PubSubClient MQTT(wifiClient);
 
-const char* BROKER_MQTT = "IP-BROKER-MQTT";
-int BROKER_PORT = 1883;                      
+const char* BROKER_MQTT = "192.168.0.34";
+const int BROKER_PORT = 1883;
 
-#define ID_MQTT  "BCI01"           
-#define TOPIC_PUBLISH "esp_client"    
-PubSubClient MQTT(wifiClient);       
+bool conectBroker = false;
+bool conectWifi = false;
 
-//Declaração das Funções
-void mantemConexoes();  
-void conectaWiFi();    
-void enviaPacote();    
+#define ID_MQTT "BCI01"
+#define TOPIC_PUBLISH "esp_client"
 
-void conectaMQTT() { 
-    while (!MQTT.connected()) {
-        Serial.print("Conectando ao Broker MQTT: ");
-        Serial.println(BROKER_MQTT);
-        if (MQTT.connect(ID_MQTT)) {
-            Serial.println("Conectado ao Broker com sucesso!");
-        } 
-        else {
-            Serial.println("Nao foi possivel se conectar ao broker.");
-            Serial.println("Nova tentatica de conexao em 5s");
-            delay(5000);
-        }
-    }
-}
-
-void enviaValores() {
-  float tensao = random(100, 200) / 10.0; 
-  float corrente = random(5, 20) / 10.0;
-
-  char mqttMessage[200];
-  sprintf(mqttMessage, "Tensao:%.2fV | Corrente:%.2fA", tensao, corrente);
-  
-  MQTT.publish(TOPIC_PUBLISH, mqttMessage);
-  delay(10000);
-}
-
-
-void conectaWiFi() {
-
-  if (WiFi.status() == WL_CONNECTED) {
-     return;
-  }
-        
-  Serial.print("Conectando-se na rede: ");
-  Serial.print(SSID);
-  Serial.println("  Aguarde!");
-
-  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI  
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(100);
-      Serial.print(".");
-  }
-  
-  Serial.println();
-  Serial.print("Conectado com sucesso, na rede: ");
-  Serial.print(SSID);  
-  Serial.print("  IP obtido: ");
-  Serial.println(WiFi.localIP()); 
-}
-
-void mantemConexoes() {
-    if (!MQTT.connected()) {
-       conectaMQTT(); 
-    }
-    
-    conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
-}
-
+// Declaração das Funções
+void mantemConexoes();
+void conectaWiFi();
+void conectaMQTT();
+void enviaValores(float tensao, float corrente);
 
 void setup() {
-       
   Serial.begin(115200);
-
   conectaWiFi();
-  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);
 }
 
 void loop() {
   mantemConexoes();
-  enviaValores();
   MQTT.loop();
+
+  if (Serial.available()) {
+    String mensagem = Serial.readStringUntil('\n');
+    if (mensagem.startsWith("Tensao:")) {
+      int posicaoInicio = mensagem.indexOf(":") + 1;
+      int posicaoFim = mensagem.indexOf("V");
+      String tensaoStr = mensagem.substring(posicaoInicio, posicaoFim);
+      float tensao = tensaoStr.toFloat();
+
+      posicaoInicio = mensagem.indexOf("Corrente:") + 9;
+      posicaoFim = mensagem.indexOf("A");
+      String correnteStr = mensagem.substring(posicaoInicio, posicaoFim);
+      float corrente = correnteStr.toFloat();
+
+      // Enviar valores para o broker MQTT
+      enviaValores(tensao, corrente);
+    }
+  }
+}
+ 
+void mantemConexoes() {
+  if (!MQTT.connected()) {
+    conectaMQTT();
+  }
+  conectaWiFi();
 }
 
+void conectaWiFi() {
+  if (WiFi.status() == WL_CONNECTED) {
+    return;
+  }
+  Serial.print("Conectando-se na rede: ");
+  Serial.print(SSID);
+  Serial.println("  Aguarde!");
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Conectado com sucesso, na rede: ");
+  Serial.print(SSID);
+  Serial.println("IP obtido: ");
+  Serial.print(WiFi.localIP());
+}
 
+void conectaMQTT() {
+  while (!MQTT.connected()) {
+    Serial.print("Conectando ao Broker MQTT: ");
+    Serial.println(BROKER_MQTT);
+    if (MQTT.connect(ID_MQTT)) {
+      Serial.println("Conectado ao Broker com sucesso!");
+    } else {
+      Serial.println("Nao foi possivel se conectar ao broker.");
+      Serial.println("Nova tentativa de conexao em 5s");
+      delay(5000);
+    }
+  }
+}
 
+void enviaValores(float tensao, float corrente) {
+  char mqttMessage[200];
+  sprintf(mqttMessage, "Tensao:%.2fV | Corrente:%.2fA", tensao, corrente);
 
+  MQTT.publish(TOPIC_PUBLISH, mqttMessage);
+  delay(500);
+}

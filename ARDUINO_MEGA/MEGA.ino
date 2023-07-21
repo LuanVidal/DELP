f1dd86d64538533cc162fe2fb664acae63f8485c
LuanVidal <65032589+LuanVidal@users.noreply.github.com>
2023-07-21 13:59:41 -0300

    Add files via upload

--- ARDUINO_MEGA/MEGA.ino
+++ ARDUINO_MEGA/MEGA.ino
@@ -17,8 +17,8 @@ void loop() {
   float tensao = analogRead(A0);
   float corrente = analogRead(A1);
 
-  float tensaoMapeada =  tensao * 0.0048875855327468;
-  float correnteMapeada = corrente * 0.0048875855327468;
+  float tensaoMapeada =  tensao * 0.0488758553274682; // 0 a 50 - 0.0488758553274682
+  float correnteMapeada = corrente * 0.2932551319648094; // 0 a 300 - 0.2932551319648094
 
   // Envia os dados para o ESP8266 via comunicação serial
   Serial3.print("Tensao:");
@@ -26,6 +26,19 @@ void loop() {
   Serial3.print("V | Corrente:");
   Serial3.print(correnteMapeada);
   Serial3.println("A");
+
+  /*
+    int acao = 1;
+    String matricula = "tcs";
+    String mac = "123-456";
+    int ordemProducao = 1;
+    int atividade = 1;
+    int material = 1;
+
+    String mensagem = "Ação: " + String(acao) + ", \"matricula\": \"" + matricula + "\", \"mac\": \"" + mac + "\", \"ordemProducao\": " + String(ordemProducao) + ", \"atividade\": " + String(atividade) + ", \"material\": " + String(material);
+    Serial3.println(mensagem);
+  
+  */
  
   delay(500); // Intervalo de envio dos dados para o ESP8266
 }
